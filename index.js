2b0e62e73e65957bb253436d993c4ce103cf877d
James Ferguson <ncjamesferguson@gmail.com>
2017-11-13 00:57:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ function subtract(a, b) {
   return a - b;
 }
 function multiply(a, b) {
-  
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function inc(n) {
+  n++;
+}
+function dec(n) {
+    n--;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
